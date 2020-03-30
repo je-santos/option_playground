3a2033ca317c5df80754c13cf270be1ae1025241
Javier E. Santos <42117435+je-santos@users.noreply.github.com>
2020-03-29 22:07:12 -0500

    Add files via upload

---
+++ stonks.py
@@ -0,0 +1,35 @@
+import yfinance as yf
+
+
+ticker = 'AAPL'
+stock  = yf.Ticker(ticker)
+
+opt_exp_dates = stock.options
+opt_date_data = stock.option_chain( opt_exp_dates[0] ) #gets the info for the first option available
+
+"""
+The option DataFrame icludes the following data:
+    
+    -Last: The price of the last trade that went through.
+    -change: how much the last price has changed since the previous close
+    -bid: price at which buyers are trying to buy (ask is the opposite)
+    -volume: how many contracts have been traded during the session. 
+    -open interest: number of open positions in the contract that have not yet been offset.
+"""
+
+opt_calls = opt_date_data[0]
+opt_puts  = opt_date_data[1]
+
+def get_avg_bid_ask(df, percent_ask=0.5):   
+    """
+    percent ask is the weight to calculate the average 
+    the default is 50/50
+    """
+    percent_bid=1-percent_ask
+    df.insert(4,'bid/ask',df['bid']*percent_bid + df['ask']*percent_ask)
+    return df
+
+for financial in [opt_calls, opt_puts]: 
+    try: 
+        get_avg_bid_ask(financial)
+    except: print('There is aready info in that df')
